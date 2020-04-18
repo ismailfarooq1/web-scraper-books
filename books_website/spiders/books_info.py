c7b4a101bdfcafb8c325aa20cfabe825ebb8492a
Ismail Farooq <ismaeelfarooq53@gmail.com>
2020-04-19 04:05:03 +0500

    Revision 1

--- books_website/spiders/books_info.py
+++ books_website/spiders/books_info.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import scrapy
 
 
@@ -6,8 +5,8 @@ class BooksInfoSpider(scrapy.Spider):
     name = 'books_info'
     allowed_domains = ['books.toscrape.com']
 
-    def start_request(self):
-        yield scrapy.Request(url='http://books.toscrape.com', callable=self.parse, headers={
+    def start_requests(self):
+        yield scrapy.Request(url='http://books.toscrape.com', callback=self.parse, headers={
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'
         })
 
@@ -15,14 +14,8 @@ class BooksInfoSpider(scrapy.Spider):
         path = response.xpath("//article[@class='product_pod']/h3")
 
         for link in path:
-            # print('PSTT')
             url = link.xpath(".//a/@href").get()
             name = link.xpath(".//a/text()").get()
-            print("HELLLO")
-            # yield {
-            #     'name': name,
-            #     'url': url
-            # }
 
             yield response.follow(url=url, callback=self.page_title, meta={'name': name },headers={
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'
@@ -33,6 +26,6 @@ class BooksInfoSpider(scrapy.Spider):
         name = response.request.meta['name']
         
         yield {
-            'name': 'hello',
+            'name': name,
         }
 
