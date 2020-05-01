4c938eb787d1d178c0a6a4d39eb42e4e32ad3b79
Ismail Farooq <ismaeelfarooq53@gmail.com>
2020-05-02 04:22:15 +0500

    Exercise complete

--- books_website/spiders/books_info.py
+++ books_website/spiders/books_info.py
@@ -14,13 +14,13 @@ class BooksInfoSpider(scrapy.Spider):
         path = response.xpath("//article[@class='product_pod']/h3")
 
         for link in path:
-            name = link.xpath(".//a/text()").get()
-            price = link.xpath("//p[@class='price_color']/text()").get()
+            name_link = link.xpath(".//a/@href").get()
+            # name = link.xpath(".//a/text()").get()
+            # price = link.xpath("//p[@class='price_color']/text()").get()
 
-            yield {
-                'name': name,
-                'price': price,
-            }
+            yield response.follow(url=name_link, callback=self.page_title, headers={
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36'
+            })
 
         next_page = response.xpath("//li[@class='next']/a/@href").get()
     
@@ -36,17 +36,14 @@ class BooksInfoSpider(scrapy.Spider):
             })
 
 
-    # def page_title(self, response):
-    #     name = response.request.meta['name']
-    #     price = response.xpath("//p[@class='price_color']/text()").get()
-    #     # description = response.xpath("//article/p/text()").get()
+    def page_title(self, response):
+        name = response.xpath("//div[@class='col-sm-6 product_main']/h1/text()").get()
+        price = response.xpath("//div[@class='col-sm-6 product_main']/p[position()=1]/text()").get()
 
-    #     # print("Second Function")
-
-    #     yield {
-    #         'name': name,
-    #         'price': price,
-    #         # 'description': description,
-    #         # 'next_link': next_link
-    #     }
+        yield {
+            'name': name,
+            'price': price,
+            # 'description': description,
+            # 'next_link': next_link
+        }
 
